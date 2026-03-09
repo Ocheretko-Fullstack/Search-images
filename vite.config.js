0eaa667be76e94ac664d89e6d260775eb9b10a0a
Ocheretko <kembridg1992@gmail.com>
2026-03-09 22:34:39 +0200

    delete outDir

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,6 @@ export default defineConfig(({ command }) => {
     root: 'src',
     build: {
       sourcemap: true,
-      outDir: '../dist',
       rollupOptions: {
         input: glob.sync('./src/*.html'),
         output: {
